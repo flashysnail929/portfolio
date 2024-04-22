acd6167c392d5729e9c45464f4bd79dbfb9b9b62
Siputyangcepat <budityo02@gmail.com>
2024-04-22 23:40:39 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex justify-content-center align-items-center header-transparent">
+
+        <nav id="navbar" class="navbar">
+        <ul>
+            <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+            <li><a class="nav-link scrollto" href="#about">About</a></li>
+            <li><a class="nav-link scrollto" href="#education">Education</a></li>
+            <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+            <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div class="hero-container">
+        <h1>Bernardus Brahmantyo</h1>
+        <h2>Undergraduate student at Bina Nusantara University</h2>
+        <a href="#about" class="btn-scroll scrollto" title="Scroll Down"><i class="bx bx-chevron-down"></i></a>
+        </div>
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+        <!-- ======= About Me Section ======= -->
+        <section id="about" class="about">
+          <div class="container">
+    
+            <div class="section-title">
+              <span>About Me</span>
+              <h2>About Me</h2>
+              <p>A highly motivated and results-oriented undergraduate student at Bina Nusantara University 
+                pursuing a Bachelor's degree in Computer Science, with a keen interest in web development. 
+                Possessing hands-on experience in building websites utilizing HTML, CSS, and JavaScript. 
+                Demonstrated proficiency in problem-solving, adaptability, and teamwork. Eager to leverage skills 
+                and knowledge to contribute to a dynamic and innovative team as a web developer</p>
+            </div>
+    
+            <div class="row">
+              <div class="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
+              <div class="col-lg-8 d-flex flex-column align-items-stretch">
+                <div class="content ps-lg-4 d-flex flex-column justify-content-center">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <ul>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Name:</strong> <span>Bernardus Brahmantyo Budisusetyo</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+62 8123 4567 8910</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Malang, Indonesia</span></li>
+                      </ul>
+                    </div>
+                    <div class="col-lg-6">
+                      <ul>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>21</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Undergraduate</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>bernardus.budisusetyo@binus.ac.id</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                      </ul>
+                    </div>
+                  </div>
+
+                <div class="section-title">
+                  <span>Skills</span>
+                  <h2>Skills</h2>
+                </div>
+
+                <div class="skills-content ps-lg-4">
+                  <div class="progress">
+                    <span class="skill">HTML <i class="val">100%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+    
+                  <div class="progress">
+                    <span class="skill">CSS <i class="val">90%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+    
+                  <div class="progress">
+                    <span class="skill">JavaScript <i class="val">75%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+    
+              </div>
+            </div>
+    
+          </div>
+        </section><!-- End About Me Section -->
+    
+        <!-- ======= My education Section ======= -->
+    <section id="education" class="education">
+        <div class="container">
+  
+          <div class="section-title">
+            <span>My education</span>
+            <h2>My education</h2>
+          </div>
+  
+          <div class="col">
+            
+              <h3 class="education-title">Education</h3>
+              <div class="education-item">
+                <h4>Undergraduate Program Computer Science</h4>
+                <h5>2022 - Now</h5>
+                <p><em>Bina Nusantara University, Malang, Indonesia</em></p>
+              </div>
+              <div class="education-item">
+                <h4> Diploma in Business Administration, Office Administration, and Export-Import with Information Technology</h4>
+                <h5>2021 - 2022</h5>
+                <p><em>Wearnes Education Center, Malang, Indonesia</em></p>
+              </div>
+            </div>
+          </div>
+  
+        </div>
+      </section><!-- End My education Section -->
+
+       <!-- ======= My Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+    
+            <div class="section-title">
+                <span>My Portfolio</span>
+                <h2>My Portfolio</h2>
+                <p>Showcase some of my recent works</p>
+            </div>
+
+            <div class="row portfolio-container">
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                  <div class="portfolio-img"><img src="assets/img/portfolio/work-1.png" class="img-fluid" alt=""></div>
+                    <div class="portfolio-info">
+                        <h4>Ratu Rosari Church Website</h4>
+                        <p>Contribute to a team of developers</p>
+                        <a href="assets/img/portfolio/work-1.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"><i class="bx bx-plus"></i></a>
+                        {{-- <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a> --}}
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                  <div class="portfolio-img"><img src="assets/img/portfolio/work-2.png" class="img-fluid" alt=""></div>
+                    <div class="portfolio-info">
+                        <h4>Redesign of the RSUD Kota Malang website</h4>
+                        <p>College Assignment</p>
+                        <a href="assets/img/portfolio/work-2.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"><i class="bx bx-plus"></i></a>
+                        {{-- <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a> --}}
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                  <div class="portfolio-img"><img src="assets/img/portfolio/work-3.png" class="img-fluid" alt=""></div>
+                    <div class="portfolio-info">
+                        <h4>Payroll Slip Calculator Application using Ms. Access</h4>
+                        <a href="assets/img/portfolio/work-3.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"><i class="bx bx-plus"></i></a>
+                        {{-- <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a> --}}
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+      <!-- ======= Contact Me Section ======= -->
+    <section id="contact" class="contact">
+        <div class="container">
+  
+          <div class="section-title">
+            <span>Contact Me</span>
+            <h2>Contact Me</h2>
+            <p>Get in touch with me</p>
+          </div>
+  
+          <div class="row">
+  
+            <div class="col-lg-12">
+  
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="info-box">
+                    <i class="bx bx-share-alt"></i>
+                    <h3>Social Profiles</h3>
+                    <div class="social-links">
+                      <a href="https://www.instagram.com/bernardbramz/" class="instagram"><i class="bi bi-instagram"></i></a>
+                      <a href="https://github.com/flashysnail929" class="github"><i class="bi bi-github"></i></a>
+                      <a href="https://www.linkedin.com/in/bernardus-brahmantyo" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="info-box mt-4">
+                    <i class="bx bx-envelope"></i>
+                    <h3>Email Me</h3>
+                    <p>bernardus.budisusetyo@binus.ac.id</p>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="info-box mt-4">
+                    <i class="bx bx-phone-call"></i>
+                    <h3>Call Me</h3>
+                    <p>+62 8123 4567 8910</p>
+                  </div>
+                </div>
+              </div>
+            </div>  
+          </div>
+  
+        </div>
+      </section><!-- End Contact Me Section -->
+
+
+
+
+
+    </main><!-- End #main -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
